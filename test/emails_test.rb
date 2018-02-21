144e1bc02d50ca233463fd2d87b1f70ffce4bf50
Jon Mahoney <jon@mailosaur.com>
2018-02-21 13:08:13 +0000

    Remove redundant check for email id

--- test/emails_test.rb
+++ test/emails_test.rb
@@ -127,8 +127,7 @@ module Mailosaur
             should "perform a spam analysis on an email" do
                 target_id = @@emails[0].id
                 result = @@client.analysis.spam(target_id)
-                assert_equal(target_id, result.email_id)
-                
+
                 result.spam_filter_results.spam_assassin.each do |rule|
                     assert_instance_of(Float, rule.score)
                     assert_not_nil(rule.rule)

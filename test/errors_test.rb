79075691122bcbf6fd50a0f692558085e25f6979
Jon M <jon@mailosaur.com>
2024-08-01 13:14:37 +0100

    Use class instance vars

--- test/errors_test.rb
+++ test/errors_test.rb
@@ -5,14 +5,14 @@ require 'shoulda/context'
 module Mailosaur
   class ErrorsTest < Test::Unit::TestCase
     setup do
-      @@api_key = ENV['MAILOSAUR_API_KEY']
-      @@base_url = ENV['MAILOSAUR_BASE_URL']
+      @api_key = ENV['MAILOSAUR_API_KEY']
+      @base_url = ENV['MAILOSAUR_BASE_URL']
 
       raise ArgumentError, 'Missing necessary environment variables - refer to README.md' if @@api_key.nil?
     end
 
     should 'Unauthorized' do
-      client = MailosaurClient.new('invalid_key', @@base_url)
+      client = MailosaurClient.new('invalid_key', @base_url)
       ex = assert_raise(Mailosaur::MailosaurError) do
         client.servers.list
         pass
@@ -21,7 +21,7 @@ module Mailosaur
     end
 
     should 'Not Found' do
-      client = MailosaurClient.new(@@api_key, @@base_url)
+      client = MailosaurClient.new(@api_key, @base_url)
       ex = assert_raise(Mailosaur::MailosaurError) do
         client.servers.get('not_found')
         pass
@@ -30,7 +30,7 @@ module Mailosaur
     end
 
     should 'Bad Request' do
-      client = MailosaurClient.new(@@api_key, @@base_url)
+      client = MailosaurClient.new(@api_key, @base_url)
       ex = assert_raise(Mailosaur::MailosaurError) do
         create_options = Mailosaur::Models::ServerCreateOptions.new
         client.servers.create(create_options)

36b791dde9081b9656a7e829adb5a4bae8ca5b97
Jon M <jon@mailosaur.com>
2024-08-01 13:00:49 +0100

    Update base URL for 'Unauthorized' test

--- test/errors_test.rb
+++ test/errors_test.rb
@@ -4,8 +4,15 @@ require 'shoulda/context'
 
 module Mailosaur
   class ErrorsTest < Test::Unit::TestCase
+    setup do
+      @@api_key = ENV['MAILOSAUR_API_KEY']
+      @@base_url = ENV['MAILOSAUR_BASE_URL']
+
+      raise ArgumentError, 'Missing necessary environment variables - refer to README.md' if @@api_key.nil?
+    end
+
     should 'Unauthorized' do
-      client = MailosaurClient.new('invalid_key')
+      client = MailosaurClient.new('invalid_key', @@base_url)
       ex = assert_raise(Mailosaur::MailosaurError) do
         client.servers.list
         pass
@@ -14,7 +21,7 @@ module Mailosaur
     end
 
     should 'Not Found' do
-      client = MailosaurClient.new(ENV['MAILOSAUR_API_KEY'])
+      client = MailosaurClient.new(@@api_key, @@base_url)
       ex = assert_raise(Mailosaur::MailosaurError) do
         client.servers.get('not_found')
         pass
@@ -23,7 +30,7 @@ module Mailosaur
     end
 
     should 'Bad Request' do
-      client = MailosaurClient.new(ENV['MAILOSAUR_API_KEY'])
+      client = MailosaurClient.new(@@api_key, @@base_url)
       ex = assert_raise(Mailosaur::MailosaurError) do
         create_options = Mailosaur::Models::ServerCreateOptions.new
         client.servers.create(create_options)
